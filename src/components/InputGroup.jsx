25470767ddbb0acc0d4179c1a8dc87523d79830d
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-10-01 15:17:55 +0200

    Changed text fields to textarea instead of input and added basic styling for input forms

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -10,15 +10,26 @@ function InputGroup({
   return (
     <>
       <label htmlFor={name}>{label}</label>
-      <input
-        onChange={onChange}
-        type={type}
-        id={name}
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        data-key={dataKey}
-      ></input>
+      {type === "textarea" ? (
+        <textarea
+          onChange={onChange}
+          id={name}
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          data-key={dataKey}
+        ></textarea>
+      ) : (
+        <input
+          onChange={onChange}
+          type={type}
+          id={name}
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          data-key={dataKey}
+        ></input>
+      )}
     </>
   );
 }
