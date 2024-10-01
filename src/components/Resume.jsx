47f32a27f9125d766e7807307c9239650f4e6be4
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-10-01 23:44:32 +0200

    Add professional experience to resume preview

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,6 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Resume({
-  personalDetails,
-  shortProfile,
-  profExperience,
-  education,
-}) {
+function Resume({ personalDetails, shortProfile, profExperience, education }) {
   return (
     <>
       <h1>Resume</h1>
@@ -14,22 +9,49 @@ function Resume({
         <p>{personalDetails.phone}</p>
         <p>{personalDetails.email}</p>
         <a href={personalDetails.linkedin}>
-          <FontAwesomeIcon icon="fa-brands fa-linkedin" size="2x"/>
+          <FontAwesomeIcon icon="fa-brands fa-linkedin" size="2x" />
         </a>
         <a href={personalDetails.github}>
-          <FontAwesomeIcon icon="fa-brands fa-github" size="2x"/>
+          <FontAwesomeIcon icon="fa-brands fa-github" size="2x" />
         </a>
         <p>{personalDetails.github}</p>
-        
       </div>
       <div className="short-profile-info">
         <h2>Short Profile at a Glance</h2>
         {Object.values(shortProfile).map((short) => {
-          return <p>{short}</p>
+          return <p>{short}</p>;
+        })}
+      </div>
+      <div className="professional-experience-info">
+        <h2>Professional Experience</h2>
+        {profExperience.map((exp) => {
+          return (
+            <div key={exp.id} className="experience-info">
+              <div className="job-info">
+                <p>{exp.startDate + " - " + exp.endDate}</p>
+                <div className="job-info">
+                  <p>{exp.jobTitle}</p>
+                  <p>
+                    {exp.company + " / " + exp.sector + " / " + exp.location}
+                  </p>
+                </div>
+              </div>
+
+              <div className="job-core-tasks">
+                <p>Core Tasks</p>
+                <p>{exp.coreTasks}</p>
+              </div>
+
+              <div className="job-impact">
+                <p>Impact</p>
+                <p>{exp.impact}</p>
+              </div>
+            </div>
+          );
         })}
       </div>
     </>
-  )
+  );
 }
 
 export default Resume;
